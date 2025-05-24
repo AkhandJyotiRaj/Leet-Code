0b0b80b3545a52c2480b35a7ff260adbaff4bf31
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-05-24 23:59:59 +0530

    Fix streak

--- 45_number.java
+++ 45_number.java
@@ -11,3 +11,17 @@ class Solution {
         return ptr;
     }
 }
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int ptr = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != val){
+                nums[ptr] = nums[i];
+                ptr++;
+            }
+        }
+        return ptr;
+    }
+}
