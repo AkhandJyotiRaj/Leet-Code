e8313b3dca6b969185b597cfb01afdc48d825c22
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-10-10 02:21:09 +0530

    add

---
+++ 268_missingnumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int acutalsum = (n*(n+1))/2;
+        int arraysum = 0;
+
+        for(int i = 0;i<n;i++){
+            arraysum = arraysum + nums[i];
+        }
+        return acutalsum - arraysum;
+    }
+}
