bc106913c55d3858a5877effb7631ba398951717
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-10-10 13:06:30 +0530

    akhand

---
+++ 26. Remove Duplicates from Sorted Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        int ptr =0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=nums[ptr]){
+                ptr++;
+                nums[ptr] = nums[i];
+
+            }
+    
+        }
+
+        return ptr+1;
+    }
+
+}
