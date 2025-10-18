b72bda212c495a3f663674827f1faf069175f91a
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-10-18 08:30:33 +0530

    changes

--- addtwonumbers.java
+++ addtwonumbers.java
@@ -1,35 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// Definition for singly-linked list.
+// public class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode() {}
+//     ListNode(int val) { this.val = val; }
+//     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// }
+
 class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(); 
-        ListNode* cur = dummy;     
-        int carry = 0; 
-        while (l1 || l2 || carry) {
-            int sum = carry;     
-            if (l1) {
-                sum += l1->val;
-                l1 = l1->next;      
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-            if (l2) {
-                sum += l2->val;
-                l2 = l2->next;        
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
             }
-
-            carry = sum / 10;               
-            cur->next = new ListNode(sum % 10); 
-            cur = cur->next;       
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
         }
-
-        return dummy->next;
+        return dummy.next;
     }
-};
+}
