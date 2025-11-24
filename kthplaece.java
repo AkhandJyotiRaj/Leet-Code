f4b542c4dfe2c85bfccbb6dbb410de7045a081bc
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-11-24 12:19:29 +0530

    khtn place

---
+++ kthplaece.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        // Base case
+        if (n == 1) return 0;
+
+        // Total length = 2^(n-1)
+        int mid = (int)Math.pow(2, n-1) / 2;
+
+        if (k <= mid) {
+            return kthGrammar(n - 1, k);
+        } else {
+            return 1 - kthGrammar(n - 1, k - mid);
+        }
+    }
+}
