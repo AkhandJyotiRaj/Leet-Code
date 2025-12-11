d64d52ffb585c3cf083e7cb93b4b6af70fd16c4b
Rahul kumar  /* A J R */ <rahulkumar108642@gmail.com>
2025-12-11 10:53:02 +0530

    Time: 4 ms (54.96%) | Memory: 45.5 MB (58.88%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,22 +1,24 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle){
-        int m = triangle.size();        
-        int[][] dp = new int[m][m];
-        dp[0][0] = triangle.get(0).get(0);
+        int m = triangle.size();
+        int[] curr = new int[m];
+        int[] prev = new int[m];
+        prev[0] = triangle.get(0).get(0);
         for(int i = 1; i < m; i++){
             int n = triangle.get(i).size();
             for(int j = 0; j < n; j++){
                 if(i == 0 && j == 0) continue;
                 else{
-                    int down = (j != n - 1) ? dp[i - 1][j] : Integer.MAX_VALUE;
-                    int diagonal = (j > 0) ? dp[i - 1][j - 1] : Integer.MAX_VALUE;
-                    dp[i][j] = triangle.get(i).get(j) + Math.min(down, diagonal);
+                    int down = (j != n - 1) ? prev[j] : Integer.MAX_VALUE;
+                    int diagonal = (j > 0) ? prev[j - 1] : Integer.MAX_VALUE;
+                    curr[j] = triangle.get(i).get(j) + Math.min(down, diagonal);
                 }
             }
+            prev = curr.clone();
         } 
         int answer = Integer.MAX_VALUE;
         for(int i = 0; i <m; i++){
-            answer = Math.min(answer, dp[m - 1][i]);
+            answer = Math.min(answer, prev[i]);
 
         }     
         return answer;  
