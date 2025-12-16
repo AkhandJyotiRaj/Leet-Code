49479d649c51aef56b49499745319d467d3d0ffc
Rahul kumar  /* A J R */ <rahulkumar108642@gmail.com>
2025-12-16 10:20:08 +0530

    Time: 1 ms (71.51%) | Memory: 44.7 MB (96.15%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,32 +1,66 @@
 class Solution {
+    public int solve(int i,int j,int n,int m,int[][] dungeon,int[][] dp){
+        if((i==n-1) && (j==m-1)){
+            if(dungeon[i][j]>0) return 1;
+            else return  Math.abs(dungeon[i][j])+1;
+        }
+
+        if(i>=n || j>=m) return Integer.MAX_VALUE;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int down=solve(i+1,j,n,m,dungeon,dp);
+        int right=solve(i,j+1,n,m,dungeon,dp);
+
+        int check=Math.min(down,right)-dungeon[i][j];
+        if(check>0){
+            return dp[i][j]=check;
+        }
+        return dp[i][j]=1;
+    }
     public int calculateMinimumHP(int[][] dungeon) {
-        int m = dungeon.length;
-        int n = dungeon[0].length;
-        
-        int[][] dp = new int[m][n];
-        
-        // Base case: princess cell
-        dp[m-1][n-1] = Math.max(1, 1 - dungeon[m-1][n-1]);
-        
-        // Fill last row
-        for (int j = n-2; j >= 0; j--) {
-            dp[m-1][j] = Math.max(1, dp[m-1][j+1] - dungeon[m-1][j]);
-        }
-        
-        // Fill last column
-        for (int i = m-2; i >= 0; i--) {
-            dp[i][n-1] = Math.max(1, dp[i+1][n-1] - dungeon[i][n-1]);
-        }
-        
-        // Fill rest of the grid
-        for (int i = m-2; i >= 0; i--) {
-            for (int j = n-2; j >= 0; j--) {
-                int minExit = Math.min(dp[i+1][j], dp[i][j+1]);
-                dp[i][j] = Math.max(1, minExit - dungeon[i][j]);
+        int n=dungeon.length;
+        int m=dungeon[0].length;
+        int[][] dp=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dp[i][j]=-1;
             }
         }
-        
-        return dp[0][0];
+
+        return solve(0,0,n,m,dungeon,dp);
     }
+
+    // public boolean solve(int i,int j,int mid,int[][] dungeon){
+    //  if(mid<=0) return false;
+    //  if(i>=dungeon.length || j>=dungeon[0].length){
+    //     return false;
+    //  }
+    //  if(i==dungeon.length-1 && j==dungeon[0].length-1){
+    //     if(mid+dungeon[i][j]>=1)return true;
+    //     else return false;
+    //  }
+
+    //  boolean down=solve(i+1,j,mid+dungeon[i][j],dungeon);
+    //  boolean right=solve(i,j+1,mid+dungeon[i][j],dungeon);
+  
+    // return down||right;
+     
+    // }
+    // public int calculateMinimumHP(int[][] dungeon) {
+    //     int low=1;
+    //     int high=(int)4e7;
+    //     int result=0;
+    //     while(low<=high){
+    //         int mid=low+(high-low)/2;
+    //         boolean check=solve(0,0,mid,dungeon);
+    //         if(check){
+    //             result=mid;
+    //             high=mid-1;
+    //         }
+    //          else low=mid+1;
+    //     }
+
+    //     return result;
+    // }
 }
-        
