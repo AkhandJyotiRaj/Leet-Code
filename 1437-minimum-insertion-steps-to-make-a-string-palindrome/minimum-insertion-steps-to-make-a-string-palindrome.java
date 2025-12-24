4c103ea4bc0e093b4f137fd6dd3e7e1400c381ca
Rahul kumar  /* A J R */ <rahulkumar108642@gmail.com>
2025-12-24 09:22:11 +0530

    Time: 23 ms (71.29%) | Memory: 46.8 MB (71.98%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[][] dp;
+
+    public int minInsertions(String s) {
+        int n = s.length();
+        dp = new int[n][n];
+        return solve(s, 0, n - 1);
+    }
+
+    public int solve(String s, int i, int j) {
+        // base cases
+        if (i > j) return 0;
+        if (i == j) return 0;
+
+        // already calculated
+        if (dp[i][j] != 0) return dp[i][j];
+
+        if (s.charAt(i) == s.charAt(j)) {
+            dp[i][j] = solve(s, i + 1, j - 1);
+        } else {
+            dp[i][j] = Math.min(
+               1 + solve(s, i + 1, j),
+               1 + solve(s, i, j - 1)
+            );
+        }
+        return dp[i][j];
+    }
+}
