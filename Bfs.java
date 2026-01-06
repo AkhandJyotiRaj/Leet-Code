fa465696eb8aecf6a6bcbacd40e564b6d0398b96
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2026-01-06 10:02:12 +0530

    akhand

---
+++ Bfs.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        int V = adj.size();
+        boolean[] visited = new boolean[V];
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        int src = 0;
+        Queue<Integer> q = new LinkedList<>();
+        visited[src] = true;
+        q.add(src);
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            res.add(curr);
+
+            for (int x : adj.get(curr)) {
+                if (!visited[x]) {
+                    visited[x] = true;
+                    q.add(x);
+                }
+            }
+        }
+        
+        return res;
+        
+    }
+}
