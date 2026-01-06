279afda1497096bd7b5814e82df38109a10b3ed1
Rahul kumar  /* A J R */ <rahulkumar108642@gmail.com>
2026-01-06 11:03:07 +0530

    Time: 2 ms (56.92%) | Memory: 46.5 MB (28.94%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int vis[]=new int[rooms.size()];
+        vis[0]=1;
+        Stack<Integer> st=new Stack<>();
+        st.push(0);
+
+        while(!st.isEmpty())
+        {
+            int node=st.pop();
+            for(int ele : rooms.get(node))
+            {
+                if(vis[ele]==0){
+                vis[ele]=1;
+                st.push(ele);
+                }
+
+            }
+        }
+        for(int i=0;i<vis.length;i++)
+        {
+            if(vis[i]==0)
+            return false;
+        }
+        return true;
+        
+    }
+}
