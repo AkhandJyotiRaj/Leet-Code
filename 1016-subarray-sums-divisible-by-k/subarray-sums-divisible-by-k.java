31fb3087d00c07512d5899230745dfaf1478dd2f
Rahul kumar  /* A J R */ <rahulkumar108642@gmail.com>
2026-02-06 10:34:15 +0530

    Time: 23 ms (79.96%) | Memory: 54.9 MB (6.07%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -5,8 +5,8 @@ class Solution {
         int end = 0;
         int count = 0;
         int sum = 0;
-        int[] remainderCount = new int[k];
-        remainderCount[0] = 1; 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);  
 
 
         for(int j = 0;j<n;j++){
@@ -15,8 +15,9 @@ class Solution {
             if(rem<0){
                 rem  +=k;
             }
-            count += remainderCount[rem];
-            remainderCount[rem]++;
+            count += map.getOrDefault(rem,0);
+            map.put(rem,map.getOrDefault(rem,0)+1);  
+            
         
         }
         return count;
