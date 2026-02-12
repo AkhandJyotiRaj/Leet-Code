0b2fbe02735eac8b0ef6d2f7009d21ce96c037c8
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2026-02-12 21:21:08 +0530

    new add

---
+++ maxizumgap.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+        if(nums.length<2) return 0;
+        int op=0;
+          for(int i =1;i<nums.length;i++)
+          {
+            op =Math.max(op,nums[i]-nums[i-1]);
+          }
+          return op;
+
+    }
+} maxizumgap {
+    
+}
