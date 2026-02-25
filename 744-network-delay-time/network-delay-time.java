085b0bd53458292e51417273588dce9a3c19af65
Rahul kumar  /* A J R */ <rahulkumar108642@gmail.com>
2026-02-25 10:20:12 +0530

    Time: 34 ms (5.38%) | Memory: 51.6 MB (6.49%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,31 +1,44 @@
 class Solution {
+    HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
     public int networkDelayTime(int[][] times, int n, int k) {
-        int result[] = new int[n + 1];
-        int int_max = Integer.MAX_VALUE;
-        Arrays.fill(result, int_max);
-        result[k] = 0;
-
-        for(int i = 0; i < n - 1; i++) {
-            boolean found = false;
-
-            for(int time[] : times) {
-                int src = time[0];
-                int dest = time[1];
-                int t = time[2];
-
-                if(result[src] == int_max) continue;
-                if(result[src] + t < result[dest]) {
-                    found = true;
-                    result[dest] = result[src] + t;
+        for (int i = 1; i <= n; i++) {
+            map.put(i, new HashMap<>());
+        }
+        for (int i = 0; i < times.length; i++) {
+            map.get(times[i][0]).put(times[i][1], times[i][2]);
+        }
+        int ans = BFS(k, n);
+        
+        return ans == 1000 ? -1 : ans;
+    }
+    public int BFS(int k, int n) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        
+        int[] timeReach = new int[n + 1];
+        Arrays.fill(timeReach, 1000);
+        timeReach[0] = 0;
+        
+        timeReach[k] = 0;
+        
+        queue.add(k);
+        
+        while (!queue.isEmpty()) {
+            int rv = queue.remove();
+            for (int nbrs : map.get(rv).keySet()) {
+                int t = map.get(rv).get(nbrs) + timeReach[rv];
+                if (t < timeReach[nbrs]) {
+                    timeReach[nbrs] = t;
+                    queue.add(nbrs);
                 }
             }
-            if(found == false) break;
         }
-        int answer = -1;
-        for(int i = 1; i < n + 1; i++) {
-            if(result[i] == int_max) return -1;
-            answer = Math.max(answer, result[i]);
+        
+        int time = 0;
+        
+        for (int i : timeReach) {
+            time = Math.max(i, time);
         }
-        return answer;
+        
+        return time;
     }
 }
