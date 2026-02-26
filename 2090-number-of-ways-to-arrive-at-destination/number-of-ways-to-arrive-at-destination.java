9decce6167bfbbfa9d727e32d77e5ab093aac1c6
Rahul kumar  /* A J R */ <rahulkumar108642@gmail.com>
2026-02-26 14:52:01 +0530

    Time: 14 ms (46.39%) | Memory: 51.3 MB (92.68%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,68 +1,50 @@
+import java.util.*;
 class Solution {
-    class Node{
-        int src;
-        int dest;
-        int wt;
-        Node(int src, int dest, int wt){
-            this.src=src;
-            this.dest=dest;
-            this.wt=wt;
-        }
-    }
-    class Pair implements Comparable<Pair>{
-        long shortestDist;
-        int node;
-        Pair(int node, long shortestDist){
-            this.shortestDist=shortestDist;
-            this.node=node;
-        }
-        public int compareTo(Pair p2){
-            return Long.compare(this.shortestDist, p2.shortestDist);
-        }
-    }
     public int countPaths(int n, int[][] roads) {
-        int MOD = (int)1e9 + 7;
-        @SuppressWarnings("unchecked")
-        ArrayList<Node>[] graph = new ArrayList[n]; 
-        for(int i=0; i<n; i++){
-            graph[i] = new ArrayList<>();
+        int MOD = 1_000_000_007;
+        List<List<long[]>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
         }
-        for(int i=0; i<roads.length; i++){
-            int u = roads[i][0];
-            int v = roads[i][1];
-            int wt = roads[i][2];
-            graph[u].add(new Node(u, v, wt));
-            graph[v].add(new Node(v, u, wt));
+        for (int[] r : roads) {
+            int u = r[0], v = r[1], w = r[2];
+            graph.get(u).add(new long[]{v, w});
+            graph.get(v).add(new long[]{u, w});
         }
 
-        boolean[] isVisited = new boolean[n];
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
         long[] dist = new long[n];
         Arrays.fill(dist, Long.MAX_VALUE);
         dist[0] = 0;
-        int[] ways = new int[n];
+
+        long[] ways = new long[n];
         ways[0] = 1;
-        
-        pq.add(new Pair(0, 0));
-        while(!pq.isEmpty()){
-            Pair curr = pq.poll();
-            if(!isVisited[curr.node]){
-                isVisited[curr.node] = true;
-                
-                for(Node neighbour : graph[curr.node]){
-                    int u = neighbour.src;
-                    int v = neighbour.dest;
-                    int w = neighbour.wt;
-                    if(dist[u]!=Integer.MAX_VALUE && dist[u] + w < dist[v]){
-                        dist[v] = dist[u] + (long)w;
-                        ways[v] = ways[u];
-                        pq.add(new Pair(v, dist[v]));
-                    }else if(dist[u]!=Integer.MAX_VALUE && dist[u] + w == dist[v]){
-                        ways[v] = (ways[v] + ways[u])%MOD;
-                    }
+
+        PriorityQueue<long[]> pq = new PriorityQueue<>(
+            (a, b) -> Long.compare(a[1], b[1])
+        );
+        pq.offer(new long[]{0, 0});
+
+        while (!pq.isEmpty()) {
+            long[] cur = pq.poll();
+            int node = (int) cur[0];
+            long d = cur[1];
+
+            if (d > dist[node]) continue;
+
+            for (long[] nei : graph.get(node)) {
+                int next = (int) nei[0];
+                long weight = nei[1];
+
+                if (dist[next] > d + weight) {
+                    dist[next] = d + weight;
+                    ways[next] = ways[node];
+                    pq.offer(new long[]{next, dist[next]});
+                } else if (dist[next] == d + weight) {
+                    ways[next] = (ways[next] + ways[node]) % MOD;
                 }
             }
         }
-        return ways[n-1];
+
+        return (int)(ways[n - 1] % MOD);
     }
 }
