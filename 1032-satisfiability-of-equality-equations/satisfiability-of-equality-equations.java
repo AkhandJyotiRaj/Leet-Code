21a728f8659ae1463b0336f7cdfebbe10eaefabd
Rahul kumar <rahulkumar108642@gmail.com>
2026-03-10 10:49:37 +0530

    Time: 1 ms (97.34%) | Memory: 43.7 MB (84.15%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -1,51 +1,57 @@
 class Solution {
-    class DSU{
-        int[] parent;
 
-        DSU(int n){
-            parent = new int[n+1];
-            for(int i = 0; i <= n; i++){
+    class DSU {
+        int[] parent;
+        int[] rank;
+
+        DSU(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) {
                 parent[i] = i;
             }
         }
 
-        int find(int x){
-            if(parent[x] == x){
-                return x;
+        int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
             }
-            return parent[x] = find(parent[x]);
+            return parent[x];
         }
 
-        void union(int a, int b){
+        void union(int a, int b) {
             int pa = find(a);
             int pb = find(b);
 
-            if(pa != pb){
+            if (pa == pb) return;
+
+            if (rank[pa] < rank[pb]) {
+                parent[pa] = pb;
+            } else if (rank[pb] < rank[pa]) {
                 parent[pb] = pa;
+            } else {
+                parent[pb] = pa;
+                rank[pa]++;
             }
         }
     }
 
     public boolean equationsPossible(String[] equations) {
-
         DSU dsu = new DSU(26);
 
-        // Step 1: process all ==
-        for(String eq : equations){
-            if(eq.charAt(1) == '='){
+        for (String eq : equations) {
+            if (eq.charAt(1) == '=') {
                 int a = eq.charAt(0) - 'a';
                 int b = eq.charAt(3) - 'a';
                 dsu.union(a, b);
             }
         }
 
-        // Step 2: check all !=
-        for(String eq : equations){
-            if(eq.charAt(1) == '!'){
+        for (String eq : equations) {
+            if (eq.charAt(1) == '!') {
                 int a = eq.charAt(0) - 'a';
                 int b = eq.charAt(3) - 'a';
-
-                if(dsu.find(a) == dsu.find(b)){
+                if (dsu.find(a) == dsu.find(b)) {
                     return false;
                 }
             }
