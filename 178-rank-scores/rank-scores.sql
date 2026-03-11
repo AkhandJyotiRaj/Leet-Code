bc0e75c716b7c5061cdb668cfc5a3fd57811e52f
Rahul kumar <rahulkumar108642@gmail.com>
2026-03-11 12:04:14 +0530

    Time: 335 ms (30.26%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,3 +1,8 @@
 # Write your MySQL query statement below
+
+WITH UniqueScores AS (
+    SELECT DISTINCT score
+    FROM Scores
+)
  select score,Dense_rank() over (order by score desc) as "rank" 
  from scores;
