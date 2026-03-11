880abf40f3be43d1ade17b2072d41be9f1c2be5f
Rahul kumar <rahulkumar108642@gmail.com>
2026-03-11 09:46:36 +0530

    Time: 342 ms (63.76%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,20 +1,22 @@
-# Write your MySQL query statement below
+SELECT 
+    visited_on,
+    SUM(amount) OVER(
+        ORDER BY visited_on 
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    
+    ROUND(
+        SUM(amount) OVER(
+            ORDER BY visited_on 
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ) / 7, 
+    2) AS average_amount
 
-select
-t2.visited_on
-,sum(c.amount) amount
-,round(sum(c.amount)/count(distinct c.visited_on),2) average_amount
-from 
+FROM
 (
-select visited_on from (
-    select
-    lead(visited_on,6) over(order by visited_on) as visited_on
-    from Customer 
-    where visited_on is not null
-    group by visited_on
-    ) t1
-where visited_on is not null
-) t2
-left join Customer c
-on c.visited_on between subdate(t2.visited_on,6) and t2.visited_on 
-group by t2.visited_on
+    SELECT visited_on, SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+) AS daily
+
+LIMIT 100000 OFFSET 6;
