5739a85f74179f50cb37299f27a8af4ae2929fad
Rahul kumar <rahulkumar108642@gmail.com>
2026-04-07 14:15:18 +0530

    Time: 1 ms (99.82%) | Memory: 49.8 MB (95.72%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,86 +1,30 @@
-// class Solution {
-//     public int possibleWays(int[][] grid, int x, int y, Integer[][]dp){
-//         // Base Case
-//         if(x==grid.length-1 && y==grid[0].length-1){ // Destination
-//             return grid[x][y]; // itself is the min
-//         }else if(x>=grid.length || y>=grid[0].length){ // Moving beyond destination
-//             return Integer.MAX_VALUE; // invalid
-//         }
-
-//         if(dp[x][y]!=null){
-//             return dp[x][y];
-//         }
-
-        
-//         // Moving Right
-//         int right = possibleWays(grid, x, y+1, dp);
-//         // Moving Down
-//         int down =  possibleWays(grid, x+1, y, dp);
-
-//         return dp[x][y] = grid[x][y] + Math.min(right, down); // take best 
-//     }
-//     public int minPathSum(int[][] grid) {
-//         Integer[][] dp = new Integer[grid.length][grid[0].length];
-//         return possibleWays(grid, 0, 0, dp);
-//     }
-// }
-
-
-// class Solution {
-//     public int minPathSum(int[][] grid) {
-        
-//         int m = grid.length;
-//         int n = grid[0].length;
-
-//         int[][] dp = new int[m][n];
-
-//         for (int k = 0; k < m; k++) {
-//             Arrays.fill(dp[k], -1);
-//         }
-
-//         return minPathSum1(grid, m - 1 , n - 1, dp);
-//     }
-
-//     public int minPathSum1(int[][] grid, int m, int n, int[][] dp) {
-//         if(m == 0 && n == 0) {
-//             return grid[0][0];
-//         } 
-//         if(m < 0 || n < 0) {
-//             return Integer.MAX_VALUE;
-//         } 
-
-//         if(dp[m][n] != -1) {
-//             return dp[m][n];
-//         }
-
-//         int up = minPathSum1(grid, m-1, n, dp);
-//         int left = minPathSum1(grid, m, n-1, dp);
-
-//         return dp[m][n] = grid[m][n] + Math.min(up, left);
-//     }
-// }
-
-
 class Solution {
-	public int minPathSum(int[][] grid) {
-
-		  // each cell of 2D array should contains the cost of path that will be minimum
-		  // from index[0][0] to that particular cell
-
-		for(int i=0; i<grid.length; i++) {
-			for(int j=0; j<grid[0].length; j++) {
-				if(i==0 && j!=0) {
-					grid[i][j] = grid[i][j] + grid[i][j-1];
-				}
-				if(j==0 && i!=0) {
-					grid[i][j] = grid[i][j] + grid[i-1][j];
-				}
-				if(i!=0 && j!=0) {
-					grid[i][j] = grid[i][j] + Math.min(grid[i-1][j], grid[i][j-1]);
-				}
-			}
-		}
-
-		return grid[grid.length-1][grid[0].length-1];
-	}
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        Integer[][] dp = new Integer[m][n]; 
+        
+        return helper(0, 0, grid, dp);
+    }
+    
+    private int helper(int i, int j, int[][] grid, Integer[][] dp) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        if (i >= m || j >= n) return Integer.MAX_VALUE;
+        
+        if (i == m - 1 && j == n - 1) {
+            return grid[i][j];
+        }
+        
+        if (dp[i][j] != null) return dp[i][j];
+        
+        int right = helper(i, j + 1, grid, dp);
+        int down = helper(i + 1, j, grid, dp);
+        
+        dp[i][j] = grid[i][j] + Math.min(right, down);
+        
+        return dp[i][j];
+    }
 }
