7c0057ef12db978bc9c6f5f40f7773aade07d4cb
Rahul kumar <rahulkumar108642@gmail.com>
2026-05-18 17:08:46 +0530

    Time: 65 ms (29.82%) | Memory: 102.7 MB (10.14%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.java
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -1,43 +1,58 @@
 class Solution {
     public int minJumps(int[] arr) {
-        int steps=0;
-        int  n=arr.length;
-        HashMap<Integer,List<Integer>> hm=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(!hm.containsKey(arr[i])){
-                List<Integer> temp =new ArrayList<>();
-                temp.add(i);
-                hm.put(arr[i],temp);
-            }
-            else{
-                List<Integer> temp=hm.get(arr[i]);
-                temp.add(i);
-                hm.put(arr[i],temp);
-            }
+        HashMap<Integer, HashSet<Integer>> numIndexMap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            numIndexMap.computeIfAbsent(arr[i], k -> new HashSet<>()).add(i);
         }
-        boolean vis[]=new boolean[n];
-        vis[0]=true;
-        Queue<Integer> qu=new LinkedList<>();
-        qu.add(0);
-        while(!qu.isEmpty()){
-            int size=qu.size();
-            for(int i=0;i<size;i++){
-                int curridx=qu.poll();
-                if(curridx==n-1)
-                    return steps;
-                List<Integer> jumptoindexes=hm.get(arr[curridx]);
-                jumptoindexes.add(curridx+1);
-                jumptoindexes.add(curridx-1);
-                for(int q:jumptoindexes){
-                    if(q>=0 && q<n && !vis[q]){
-                        vis[q]=true;
-                        qu.add(q);
+        boolean[] visited = new boolean[arr.length];
+        boolean[] added = new boolean[arr.length];
+        int[] jumps = new int[arr.length];
+        int size = arr.length;
+        Arrays.fill(jumps, Integer.MAX_VALUE);
+        jumps[0] = 0;
+        ArrayList<Integer> jumpList = new ArrayList<>();
+        jumpList.add(0);
+        added[0] = true;
+        for (int i = 0; i < jumpList.size(); i++) {
+            int currIndex = jumpList.get(i);
+            if(visited[currIndex]) {
+                continue;
+            }
+            visited[currIndex] = true;
+            if (currIndex - 1 > 0 && !added[currIndex - 1] && !visited[currIndex - 1]) {
+                jumpList.add(currIndex - 1);
+                if(jumps[currIndex - 1] > jumps[currIndex] + 1) {
+                    jumps[currIndex - 1] = jumps[currIndex] + 1;
+                    added[currIndex - 1] = true;
+                }
+            }
+            if (currIndex + 1 < size && !added[currIndex + 1] && !visited[currIndex + 1]) {
+                jumpList.add(currIndex + 1);
+                if(jumps[currIndex + 1]> jumps[currIndex] + 1) {
+                    jumps[currIndex + 1] = jumps[currIndex] + 1;
+                    added[currIndex + 1] = true;
+                }
+                if(currIndex + 1 == size - 1) {
+                    break;
+                }
+            }
+            HashSet<Integer> numIndexSet;
+            if (numIndexMap.containsKey(arr[currIndex])) {
+                numIndexSet = numIndexMap.get(arr[currIndex]);
+                for(Integer index : numIndexSet) {
+                    if (!added[index] && !visited[index]) {
+                        jumpList.add(index);
+                        if(jumps[index] > jumps[index] + 1) {
+                            jumps[index] = jumps[currIndex] + 1;
+                        }
+                        if(index == size - 1) {
+                            break;
+                        }
                     }
                 }
-                jumptoindexes.clear();
+                numIndexMap.remove(arr[currIndex]);
             }
-            steps++;
         }
-        return -1;
+        return jumps[size - 1];
     }
 }
