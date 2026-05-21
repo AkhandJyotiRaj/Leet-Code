40db4bbc480d42a240be8fa36538b66d55502e0b
Rahul kumar <rahulkumar108642@gmail.com>
2026-05-21 16:07:39 +0530

    Time: 20 ms (78.25%) | Memory: 53.9 MB (50.38%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,48 +1,20 @@
-// class Solution {
-//     public int longestCommonSubsequence(String text1, String text2) {
-//         return solve(text1, text2, text1.length(), text2.length());
-//     }
-//     public static int solve(String t1, String t2, int m,int n){
-//         if(m==0 || n==0) return 0;
-//         if(t1.charAt(m-1)==t2.charAt(n-1)){
-//             return 1 + solve(t1, t2, m-1, n-1);
-//         }else{
-//             return 0 + Math.max(solve(t1, t2, m-1, n), solve(t1, t2, m, n-1));
-//         }
-//     }
-// }
-
-
-class Solution {   //memorization
+class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
-        
-        int[][] dp = new int[m + 1][n + 1];
+        int n = text1.length();
+        int m = text2.length();
 
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                dp[i][j] = -1;
+        int[][] dp = new int[n+1][m+1];
+
+        for( int i = 1 ;i<=n; i++){
+            for( int j = 1 ; j<=m; j++){
+                if( text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }   
+                else {
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-        
-        return solve(text1, text2, m, n, dp);
-    }
-
-    public static int solve(String t1, String t2, int m, int n, int[][] dp) {
-        if (m == 0 || n == 0) return 0;
-        
-        if (dp[m][n] != -1) return dp[m][n];
-        
-        if (t1.charAt(m - 1) == t2.charAt(n - 1)) {
-            dp[m][n] = 1 + solve(t1, t2, m - 1, n - 1, dp);
-        } else {
-            dp[m][n] = Math.max(
-                solve(t1, t2, m - 1, n, dp),
-                solve(t1, t2, m, n - 1, dp)
-            );
-        }
-        
-        return dp[m][n];
+        return dp[n][m];
     }
 }
