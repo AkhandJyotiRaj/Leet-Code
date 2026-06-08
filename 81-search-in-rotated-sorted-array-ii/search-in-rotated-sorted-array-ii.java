14a5be0d6c1361e54f8ea356a5f2b4df6eb58337
Rahul kumar <rahulkumar108642@gmail.com>
2026-06-08 14:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (26.15%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,42 +1,32 @@
 class Solution {
-    public boolean search(int[] nums, int target) 
-    {
-        int l = 0;
-        int h = nums.length-1;
-        while(l<=h)
-        {
-            int mid = (h+l)/2;
-            if(nums[mid]==target) return true;
-            if(nums[l]==nums[mid] && nums[mid]==nums[h])
-            {
-                l++;
-                h--;
-                continue;
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while(right >= left){
+           int mid = left + ( right - left ) / 2;
+
+            if(nums[mid] == target) return true;
+
+            if(nums[mid] == nums[left] && nums[mid] == nums[right]){
+                left++;
+                right--;
             }
-            if(nums[l]<=nums[mid])
-            {
-                if(nums[l]<=target && nums[mid]>=target)
-                {
-                    h=mid-1;
+
+           else if(nums[left] <= nums[mid]){ // target is in sorted left half
+                if(target < nums[mid] && target >= nums[left]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
                 }
-                else
-                {
-                    l=mid+1;
                 }
+            else{ // target is in sorted right half
+                if(target > nums[mid] && target <= nums[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
             }
-            else
-            {
-                if(nums[mid]<=target && nums[h]>=target)
-                {
-                    l=mid+1;
-                }
-                else
-                {
-                    h=mid-1;
-                }
             }
         }
         return false;
     }
-//please upvote...
 }
