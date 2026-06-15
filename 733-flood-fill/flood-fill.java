7e6f488d254845a3f629508c2b97797ff8972dd2
Rahul kumar <rahulkumar108642@gmail.com>
2026-06-16 01:29:22 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (89.11%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,63 +1,29 @@
 class Solution {
-    class Pair{
-        int first;
-        int second;
-        Pair(int first, int second){
-            this.first = first;
-            this.second = second;
-        }
-    }
-    public void BFS(int [][]image, int visited[][], int sr, int sc,int color){
-        int m = image.length;
-        int n = image[0].length;
-        visited[sr][sc] = 1;
-        int initial = image[sr][sc];
-        image[sr][sc] = color;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr,sc));
-        while(!q.isEmpty()){
-            Pair node =  q.remove();
-            int first = node.first;
-            int second = node.second;
-
-            // Right
-            if (second < n - 1 && image[first][second + 1] == initial  && visited[first][second + 1] == 0) {
-                q.add(new Pair(first, second + 1));
-                image[first][ second + 1] = color;
-                visited[first][second + 1] = 1;
-            }
-
-            // Left
-            if (second > 0 && image[first][second - 1] == initial && visited[first][second - 1] == 0) {
-                q.add(new Pair(first, second - 1));
-                image[first][ second - 1] = color;
-                visited[first][second - 1] = 1;
-            }
-
-            // Down
-            if (first < m - 1 && image[first + 1][second] == initial && visited[first + 1][second] == 0) {
-                q.add(new Pair(first + 1, second));
-                image[first + 1][second] = color;
-                visited[first + 1][second] = 1;
-            }
-
-            // Up
-            if (first > 0 && image[first - 1][second] == initial && visited[first - 1][second] == 0) {
-                q.add(new Pair(first - 1, second));
-                image[first - 1][second] = color;
-                visited[first - 1][second] = 1;
-            }
-
-            
-        }
-    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length;
-        int n = image[0].length;
-        int visited[][] = new int[m][n];
+        int original = image[sr][sc];
 
-        BFS(image, visited, sr, sc, color);
+        if (original == color) {
+            return image;
+        }
 
+        dfs(image, sr, sc, original, color);
         return image;
+    }
+
+    public void dfs(int[][] image, int r, int c, int original, int color) {
+        if (r < 0 || c < 0 || r >= image.length || c >= image[0].length) {
+            return;
+        }
+
+        if (image[r][c] != original) {
+            return;
+        }
+
+        image[r][c] = color;
+
+        dfs(image, r + 1, c, original, color); // down
+        dfs(image, r - 1, c, original, color); // up
+        dfs(image, r, c + 1, original, color); // right
+        dfs(image, r, c - 1, original, color); // left
     }
 }
