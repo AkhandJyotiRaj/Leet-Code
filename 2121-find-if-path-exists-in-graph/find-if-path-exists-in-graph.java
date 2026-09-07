42853edc9a00ec1b39bba48cf7c733e9615e619c
Rahul kumar <rahulkumar108642@gmail.com>
2026-09-07 06:37:48 +0530

    Time: 44 ms (93.32%) | Memory: 282.7 MB (39.49%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,41 +1,62 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         
-
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
         boolean visited[] = new boolean[n];
         Queue<Integer> q = new LinkedList<>();
 
-
-        for(int i = 0 ;i<n;i++){
+        for(int i = 0;i<n;i++){
             adj.add(new ArrayList<>());
         }
 
         for(int[] edge:edges){
             int u = edge[0];
             int v = edge[1];
+
             adj.get(u).add(v);
             adj.get(v).add(u);
-
         }
 
+
         q.offer(source);
-        visited[source] =  true;
+        visited[source] = true;
 
         while(!q.isEmpty()){
             int node = q.poll();
+
             if(node == destination){
                 return true;
             }
-            for(int neigh : adj.get(node)){
+            for(int neigh:adj.get(node)){
                 if(!visited[neigh]){
                     visited[neigh] = true;
                     q.offer(neigh);
                 }
             }
+
         }
         return false;
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
