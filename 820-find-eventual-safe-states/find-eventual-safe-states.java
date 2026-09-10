3893646d43c2ae116066d2b8e95825e2eec55192
Rahul kumar <rahulkumar108642@gmail.com>
2026-09-10 06:11:45 +0530

    Time: 4 ms (99.63%) | Memory: 63.9 MB (19.73%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,62 +1,54 @@
-import java.util.*;
-
 class Solution {
-
     public List<Integer> eventualSafeNodes(int[][] graph) {
-
+        
         int n = graph.length;
 
-        // Step 1: Reverse Graph
-        List<List<Integer>> reverse = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            reverse.add(new ArrayList<>());
-        }
+        boolean visited[] = new boolean[n];
+        boolean path[] = new boolean[n];
+        boolean find[] = new boolean[n];
 
-        // Step 2: Outdegree array
-        int[] outdegree = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            outdegree[i] = graph[i].length;
-
-            for (int neighbor : graph[i]) {
-                reverse.get(neighbor).add(i);
+        for(int i = 0;i<n;i++){
+            if(!visited[i]){
+                dfs(i,graph,visited,path,find);
             }
         }
-
-        // Step 3: Queue for terminal nodes
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            if (outdegree[i] == 0) {
-                q.offer(i);
-            }
-        }
-
-        boolean[] safe = new boolean[n];
-
-        // Step 4: BFS
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            safe[curr] = true;
-
-            for (int parent : reverse.get(curr)) {
-                outdegree[parent]--;
-
-                if (outdegree[parent] == 0) {
-                    q.offer(parent);
-                }
-            }
-        }
-
-        // Step 5: Collect result
-        List<Integer> ans = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            if (safe[i]) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0;i<n;i++){
+            if(find[i]){
                 ans.add(i);
             }
         }
-
         return ans;
+
     }
+
+    public boolean dfs(int node, int[][] graph, boolean visited[], boolean path[],boolean find[]){
+
+        visited[node] = true;
+        path[node] = true;
+
+        for(int neigh:graph[node]){
+            if(!visited[neigh]){
+                if(dfs(neigh,graph,visited,path,find)){
+                    return true;
+                }
+            }
+            else if(path[neigh]){
+                return true;
+            }
+        }
+        path[node] = false;
+        find[node]= true;
+        return false;
+
+    }
+
 }
+
+
+
+
+
+
+
+
