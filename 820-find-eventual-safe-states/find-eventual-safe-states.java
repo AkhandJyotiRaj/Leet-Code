8d67138568c4c2d0346db70fd7444ff428ba6cac
Rahul kumar <rahulkumar108642@gmail.com>
2026-09-10 06:12:02 +0530

    Time: 12 ms (64.54%) | Memory: 61.6 MB (61.80%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -3,13 +3,25 @@ class Solution {
         
         int n = graph.length;
 
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<graph[i].length;j++){
+                int v = graph[i][j];
+                adj.get(i).add(v);
+            }
+        }
+
         boolean visited[] = new boolean[n];
         boolean path[] = new boolean[n];
         boolean find[] = new boolean[n];
 
         for(int i = 0;i<n;i++){
             if(!visited[i]){
-                dfs(i,graph,visited,path,find);
+                dfs(i,adj,visited,path,find);
             }
         }
         ArrayList<Integer> ans = new ArrayList<>();
@@ -22,14 +34,14 @@ class Solution {
 
     }
 
-    public boolean dfs(int node, int[][] graph, boolean visited[], boolean path[],boolean find[]){
+    public boolean dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean visited[], boolean path[],boolean find[]){
 
         visited[node] = true;
         path[node] = true;
 
-        for(int neigh:graph[node]){
+        for(int neigh:adj.get(node)){
             if(!visited[neigh]){
-                if(dfs(neigh,graph,visited,path,find)){
+                if(dfs(neigh,adj,visited,path,find)){
                     return true;
                 }
             }
