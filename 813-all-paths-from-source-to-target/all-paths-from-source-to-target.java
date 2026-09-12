850e452bcd5a6a1f284e302d394ef6e62360fd14
Rahul kumar <rahulkumar108642@gmail.com>
2026-09-12 07:42:35 +0530

    Time: 2 ms (90.40%) | Memory: 47.9 MB (41.02%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,20 +1,29 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> ans = new LinkedList();
-        List<Integer> current = new ArrayList();
-        current.add(0);
-        dfs(0,current,graph,graph.length-1,ans);
-        return ans; 
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> path = new ArrayList<>();
+        path.add(0);
+        dfs(0,graph,ans,path);
+        return ans;
     }
-    private void dfs(int src, List<Integer> current, int graph[][], int dest, List<List<Integer>> ans){
-        if(src == dest){
-            ans.add(new ArrayList(current));
-            return;
+        public void dfs(int node,int[][] graph,List<List<Integer>> ans, ArrayList<Integer> path ){
+
+            if(node == graph.length -1){
+                ans.add(new ArrayList<>(path));
+                return;
+            }
+
+            for(int neigh:graph[node]){
+                path.add(neigh);
+                dfs(neigh,graph,ans,path);
+                path.remove(path.size()-1);
+            }
         }
-        for(int n : graph[src]){
-            current.add(n);
-            dfs(n,current,graph,dest,ans);
-            current.remove(current.size()-1);
-        }
-    }
+        
+
+
+
+    
 }
+
