04f658702e293adcacb270afdf3ad8f2b3313521
Rahul kumar <rahulkumar108642@gmail.com>
2026-09-13 07:29:49 +0530

    Time: 2 ms (87.95%) | Memory: 44.3 MB (39.69%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,68 +1,59 @@
 class Solution {
-
-    class Cell {
-        int row;
-        int col;
-        public Cell(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
-
-    private int n;
-    private int m;
-
-    private static int[][] directions = {
-        {-1, 0},
-        {0, 1},
-        {1, 0},
-        {0, -1}
-    };
-
-    private boolean isInsideGrid(int i, int j) {
-        return (i >= 0 && i < n && j >= 0 && j < m);
-    }
-
     public int orangesRotting(int[][] grid) {
-        n = grid.length;
-        m = grid[0].length;
-
-        Queue<Cell> queue = new LinkedList<>();
-        int[][] timer = new int[n][m];
-        boolean[][] rotten = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    queue.add(new Cell(i, j));
-                    rotten[i][j] = true;
+        
+        int n = grid.length;
+        int m = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+        int time = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j] == 2){
+                    q.offer(new int[]{i,j});
+                }
+                else if(grid[i][j] == 1){
+                    fresh++;
                 }
             }
         }
 
-        while (!queue.isEmpty()) {
-            Cell curr = queue.poll();
-            for (int dir = 0; dir < 4; dir++) {
-                int row = curr.row + directions[dir][0];
-                int col = curr.col + directions[dir][1];
+        int dir[][] = {
+            {-1,0},
+            {1,0},
+            {0,-1},
+            {0,1}
+        };
 
-                if (isInsideGrid(row, col) && !rotten[row][col] && grid[row][col] == 1) {
-                    rotten[row][col] = true;
-                    timer[row][col] = 1 + timer[curr.row][curr.col];
-                    queue.add(new Cell(row, col));
+        while(!q.isEmpty() && fresh > 0){
+            int size = q.size();
+            for(int k = 0;k<size;k++){
+                int [] curr = q.poll();
+                int i = curr[0];
+                int j = curr[1];
+
+                for(int d = 0;d<4;d++){
+                    int ni = i + dir[d][0];
+                    int nj = j + dir[d][1];
+                    
+                    if(ni>=0 && ni< n && nj>=0 && nj<m && grid[ni][nj] == 1){
+                        grid[ni][nj] = 2;
+                        fresh--;
+                        q.offer(new int[]{ni,nj});
+                    }
                 }
             }
+            time++;
         }
 
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1 && !rotten[i][j])
-                    return -1;
-                res = Math.max(res, timer[i][j]);
-            }
-        }
 
-        return res;
+
+
+
+
+        if(fresh >0){
+            return -1;
+        }
+        return time;
+
     }
 }
